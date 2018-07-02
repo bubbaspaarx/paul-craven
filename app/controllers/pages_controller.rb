ad03a48b5c793941e4a1636a5e3fb40840b52cb3
Edward Sparks <edd_sparks@hotmail.com>
2018-07-02 19:17:44 +0100

    blog section added, issue with links back to the main page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def blogs
+    require "rss"
+    require 'open-uri'
+    rss_results = []
+    rss = RSS::Parser.parse(open("http://medium.com/feed/@rshotton?format=xml").read, false).items
+    rss.each do |result|
+      image = result.content_encoded.to_s.html_safe.match(/http([\w\W]+?)(jpeg|gif|jpg|png|tiff|JPEG|JPG|TIFF|PNG)/)
+      description = result.content_encoded.to_s.html_safe.gsub(/<figure>([\w\W]+?)<\/figure>/, '')
+      result = { title: result.title, date: result.pubDate, link: result.link, description: result.description, content: description, image: image }
+      rss_results.push(result)
+    end
+    @blogs = rss_results
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
+  get 'blogs', to: 'pages#blogs'
+
   post 'message', to: 'messages#create', as: 'new_message'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
