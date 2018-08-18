c838077d877665f55ea9a9b3f12af68efdaa0353
Edward Sparks <edd_sparks@hotmail.com>
2018-08-18 01:58:22 +0100

    minor changes to the file

--- app/javascript/components/recaptcha.js
+++ app/javascript/components/recaptcha.js
@@ -1,10 +1,10 @@
 const enableBtn = () => {
-  // document.getElementById("button1").style.display = "block";
   document.getElementById("button1").style.display = "block";
+  document.getElementById("button2").style.display = "block";
 }
 
-// document.getElementById("button1").style.display = "none";
 document.getElementById("button1").style.display = "none";
+document.getElementById("button2").style.display = "none";
 
 const recaptcha = document.querySelectorAll(".g-recaptcha")
 
